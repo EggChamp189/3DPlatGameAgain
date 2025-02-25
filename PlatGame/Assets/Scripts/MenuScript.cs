5c7718a8dd4dfed3e30f1c36eb6740cf013083fd
eggchamp189 <tetra.gee@mymail.champlain.edu>
2025-02-25 16:17:52 -0500

    Post Finish Changes
    I made the game slightly easier and have a more comprehensive tutorial on the main screen so its easier to play. I do not intend this to be a rage game at all, I can consistently win.

--- PlatGame/Assets/Scripts/MenuScript.cs
+++ PlatGame/Assets/Scripts/MenuScript.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour
 {
     public TMP_Text text;
-    public static string toDisplay = "Controls: WASD to move, Left Click to attack, Space to Jump";
+    public static string toDisplay = "Controls: WASD to move, Left Click to attack, Left Shift to run, Space to Jump. Esc allows you to click out of the screen. Attack while moving to spin forward! Attack in midair to bounce on enemies and boxes! Jump on the red springs to get launched. You only have 3 lives for three levels. Good Luck!";
     public static int screen = 0;
     public static int highscore = 0;
 
@@ -29,7 +29,7 @@ public class MenuScript : MonoBehaviour
             // lost
             2 => "You Lost... Your highscore is " + highscore + " Coins.",
             // default
-            _ => "Controls: WASD to move, Left Click to attack, Space to Jump",
+            _ => "Controls: WASD to move, Left Click to attack, Left Shift to run, Space to Jump. Esc allows you to click out of the screen. Attack while moving to spin forward! Attack in midair to bounce on enemies and boxes! Jump on the red springs to get launched. You only have 3 lives for three levels. Good Luck!",
         };
     }
 
